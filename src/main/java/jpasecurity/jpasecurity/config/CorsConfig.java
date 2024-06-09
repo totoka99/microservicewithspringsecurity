e1494a9a0032b2b939f8af25c869800dfd528561
szala <szalaytamas90@gmail.com>
2024-06-10 00:09:29 +0200

    cors enabled

--- src/main/java/jpasecurity/jpasecurity/config/CorsConfig.java
+++ src/main/java/jpasecurity/jpasecurity/config/CorsConfig.java
@@ -1,19 +1,21 @@
 package jpasecurity.jpasecurity.config;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 public class CorsConfig implements CorsConfigurationSource {
-        @Override
-        public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-            CorsConfiguration config = new CorsConfiguration();
-            config.addAllowedOrigin("http://localhost:4200");
-            config.addAllowedMethod("*");
-            config.addAllowedHeader("*");
-            config.setAllowCredentials(true);
-            return config;
-        }
+    @Override
+    @Bean
+    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOriginPattern("*");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        config.setAllowCredentials(true);
+        return config;
+    }
 }
