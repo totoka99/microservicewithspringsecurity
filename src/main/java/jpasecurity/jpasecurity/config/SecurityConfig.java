f4862120ec36914afc7a814e8f028ecb3b3f65c1
szala <szalaytamas90@gmail.com>
2024-06-10 00:06:52 +0200

    added jwt to the security

--- src/main/java/jpasecurity/jpasecurity/config/SecurityConfig.java
+++ src/main/java/jpasecurity/jpasecurity/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package jpasecurity.jpasecurity.config;
 
 
+import jpasecurity.jpasecurity.service.JpaUserDetailsManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,76 +9,59 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
     private final CorsConfig corsConfig;
+    private final JpaUserDetailsManager jpaUserDetailsManager;
 
-    //    private final JpaUserDetailsManager jpaUserDetailsManager;
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        Map<String, PasswordEncoder> encoders = new HashMap<>();
+        encoders.put("bcrypt", new BCryptPasswordEncoder());
+        return new DelegatingPasswordEncoder("bcrypt", encoders);
     }
 
-
     @Bean
-    public AuthenticationManager authenticationManager (AuthenticationProvider authenticationProvider){
+    public AuthenticationManager authenticationManager(AuthenticationProvider authenticationProvider) {
         return new ProviderManager(authenticationProvider);
     }
+
     @Bean
-    public AuthenticationProvider authenticationProvider(InMemoryUserDetailsManager inMemoryUserDetailsManager, PasswordEncoder passwordEncoder) {
+    public AuthenticationProvider authenticationProvider(JpaUserDetailsManager jpaUserDetailsManager, PasswordEncoder passwordEncoder) {
         DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsService(inMemoryUserDetailsManager);
+        authenticationProvider.setUserDetailsService(jpaUserDetailsManager);
         authenticationProvider.setPasswordEncoder(passwordEncoder);
         return authenticationProvider;
     }
 
     @Bean
-    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
-        UserDetails user = User.withUsername("user1")
-                .password(passwordEncoder().encode("user"))
-                .roles("USER")
-                .build();
-        UserDetails admin = User.withUsername("admin")
-                .password(passwordEncoder().encode("admin"))
-                .roles("ADMIN")
-                .build();
-        return new InMemoryUserDetailsManager(user, admin);
-    }
-
-
-    @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.csrf().disable()
-                .authorizeHttpRequests(auth ->
-                        auth
-                                .requestMatchers("/api/auth/sign-in").permitAll()
-                                .requestMatchers("/api/auth/string").permitAll()
-                                .requestMatchers("/api/registration", "/logout").permitAll()
-                                .requestMatchers("/api/user/**").hasAuthority("SCOPE_ROLE_ADMIN")
-                                .requestMatchers("/api/note/**").hasRole("USER")
-                                .anyRequest().authenticated()
-                )
-                .userDetailsService(inMemoryUserDetailsManager())
+        return httpSecurity.csrf().disable().authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/user/user-details").authenticated()
+                        .requestMatchers("/api/auth/sign-in").permitAll()
+                        .requestMatchers("/api/auth/registration", "/logout").permitAll()
+                        .requestMatchers("/api/user/**").hasAnyAuthority("SCOPE_ROLE_ADMIN", "ROLE_ADMIN")
+                        .requestMatchers("/api/note/**").hasAnyAuthority("SCOPE_ROLE_USER", "ROLE_USER")
+                        .requestMatchers("/swagger-ui/**", "/swagger-resources/**", "/v3/api-docs/**").permitAll()
+                        .anyRequest().authenticated())
+                .userDetailsService(jpaUserDetailsManager)
 //                .addFilterBefore(new CookieFilter(), UsernamePasswordAuthenticationFilter.class)
-                .cors().configurationSource(corsConfig).and()
-                .logout().deleteCookies("JSESSIONID")
+                .cors().configurationSource(corsConfig)
                 .and()
                 .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
                 .build();
