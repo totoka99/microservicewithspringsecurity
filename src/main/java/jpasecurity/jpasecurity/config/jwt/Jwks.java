e9199f4fa18dd39e9c41d014d6c140db05c30cf0
szala <szalaytamas90@gmail.com>
2024-06-10 00:00:48 +0200

    jwt token , + prepared for forgotten password

--- src/main/java/jpasecurity/jpasecurity/config/jwt/Jwks.java
+++ src/main/java/jpasecurity/jpasecurity/config/jwt/Jwks.java
@@ -1,12 +1,14 @@
 package jpasecurity.jpasecurity.config.jwt;
 
 import com.nimbusds.jose.jwk.RSAKey;
+import org.springframework.stereotype.Component;
 
 import java.security.KeyPair;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 import java.util.UUID;
 
+@Component
 public class Jwks {
 
     private Jwks() {
