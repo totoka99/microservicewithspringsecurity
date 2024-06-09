cfee25f7ac0deef595f3f68ccf515f7d5c3006c0
szala <szalaytamas90@gmail.com>
2024-06-10 00:01:29 +0200

    refactor

--- src/main/java/jpasecurity/jpasecurity/controller/NoteController.java
+++ src/main/java/jpasecurity/jpasecurity/controller/NoteController.java
@@ -1,10 +1,8 @@
 package jpasecurity.jpasecurity.controller;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import jpasecurity.jpasecurity.model.dto.note.CreateNoteDto;
-import jpasecurity.jpasecurity.model.dto.note.NoteDto;
-import jpasecurity.jpasecurity.model.dto.note.UpdateNoteDto;
+import jpasecurity.jpasecurity.controller.note.CreateNoteDto;
+import jpasecurity.jpasecurity.controller.note.NoteDto;
+import jpasecurity.jpasecurity.controller.note.UpdateNoteDto;
 import jpasecurity.jpasecurity.service.NoteService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -19,39 +17,33 @@ public class NoteController {
     private final NoteService noteService;
 
     @GetMapping("/{noteId}")
-    @Operation(summary = "Return example's data")
     @ResponseStatus(HttpStatus.OK)
-    @ApiResponse(responseCode = "200")
-    public NoteDto getExampleById(@PathVariable Long exampleId) {
-        return noteService.findById(exampleId);
+    public NoteDto getExampleById(@PathVariable Long noteId) {
+        return noteService.findById(noteId);
     }
 
     @GetMapping
-    public List<NoteDto> getAllNote (){
+    @ResponseStatus(HttpStatus.OK)
+    public List<NoteDto> getAllNote() {
         return noteService.findAllNote();
     }
 
     @PostMapping
-    @Operation(summary = "Create new example")
     @ResponseStatus(HttpStatus.CREATED)
-    @ApiResponse(responseCode = "201")
-    public NoteDto createExample(@RequestBody CreateNoteDto createNoteDto) {
-        return noteService.saveNewExample(createNoteDto);
+    public NoteDto createNote(@RequestBody CreateNoteDto createNoteDto) {
+        return noteService.saveNewNote(createNoteDto);
     }
 
-    @PatchMapping("/name/{noteId}")
-    @Operation(summary = "Update example's name")
-    @ResponseStatus(HttpStatus.ACCEPTED)
-    @ApiResponse(responseCode = "202")
-    public void updateExampleName(@RequestBody UpdateNoteDto updateNoteDto, @PathVariable Long exampleId) {
-        noteService.updateExampleName(updateNoteDto, exampleId);
+
+    @PutMapping()
+    public NoteDto updateNote(@RequestBody UpdateNoteDto updateNoteDto) {
+        return noteService.updateNote(updateNoteDto);
     }
 
-    @PatchMapping("/description/{noteId}")
-    @Operation(summary = "Update example's description")
-    @ResponseStatus(HttpStatus.ACCEPTED)
-    @ApiResponse(responseCode = "202")
-    public void updateExampleDescription(@RequestBody UpdateNoteDto updateNoteDto, @PathVariable Long exampleId) {
-        noteService.updateExampleDescription(updateNoteDto, exampleId);
+
+    @DeleteMapping("/{id}")
+    public void deleteNoteById(@PathVariable Long id) {
+        System.out.println(id);
+        noteService.deleteNoteById(id);
     }
 }
--- src/main/java/jpasecurity/jpasecurity/repository/NoteRepository.java
+++ src/main/java/jpasecurity/jpasecurity/repository/NoteRepository.java
@@ -1,9 +1,13 @@
 package jpasecurity.jpasecurity.repository;
 
-import jpasecurity.jpasecurity.model.entity.Note;
+import jpasecurity.jpasecurity.service.model.entity.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface NoteRepository extends JpaRepository<Note, Long> {
+
+    List<Note> findByUser_Id(Long id);
 }
