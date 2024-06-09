419db1bdf5e82dc4af59ebbc9734f32ac76478a3
szala <szalaytamas90@gmail.com>
2024-06-09 23:53:10 +0200

    import optimization

--- src/main/java/jpasecurity/jpasecurity/service/AuthService.java
+++ src/main/java/jpasecurity/jpasecurity/service/AuthService.java
@@ -1,21 +1,12 @@
 package jpasecurity.jpasecurity.service;
 
-import jakarta.servlet.http.HttpSession;
-import jpasecurity.jpasecurity.config.SecurityUser;
-import jpasecurity.jpasecurity.model.ModelMapper;
-import jpasecurity.jpasecurity.model.dto.user.LoginUserDto;
-import jpasecurity.jpasecurity.model.dto.user.UserDto;
+import jpasecurity.jpasecurity.controller.user.LoginUserDto;
+import jpasecurity.jpasecurity.service.model.ModelMapper;
 import lombok.RequiredArgsConstructor;
-import nonapi.io.github.classgraph.json.JSONUtils;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -25,11 +16,11 @@ public class AuthService {
     private final JpaUserDetailsManager jpaUserDetailsManager;
     private final ModelMapper mapper;
 
-    public Authentication authenticateLoginRequest(LoginUserDto loginUserDto, HttpSession httpSession) {
-            Authentication authentication = authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(loginUserDto.getUsername(), loginUserDto.getPassword())
-            );
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-            return authentication;
+    public Authentication authenticateLoginRequest(LoginUserDto loginUserDto) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginUserDto.getUsername(), loginUserDto.getPassword())
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return authentication;
     }
 }
