8b05310f4c9b3e778d6c24b12c2212a763654eff
szala <szalaytamas90@gmail.com>
2024-06-10 00:01:09 +0200

    setter added

--- src/main/java/jpasecurity/jpasecurity/controller/user/LoginUserDto.java
+++ src/main/java/jpasecurity/jpasecurity/controller/user/LoginUserDto.java
@@ -1,8 +1,10 @@
-package jpasecurity.jpasecurity.model.dto.user;
+package jpasecurity.jpasecurity.controller.user;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class LoginUserDto {
     private String username;
     private String password;
