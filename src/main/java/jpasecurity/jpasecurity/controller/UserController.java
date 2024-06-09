1a7cd8d7a3ea48fba212028f04711029ba30a013
szala <szalaytamas90@gmail.com>
2024-06-10 00:03:21 +0200

    refactor

--- src/main/java/jpasecurity/jpasecurity/controller/UserController.java
+++ src/main/java/jpasecurity/jpasecurity/controller/UserController.java
@@ -1,13 +1,10 @@
 package jpasecurity.jpasecurity.controller;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
-import jpasecurity.jpasecurity.expcetion.UsernameIsTakenException;
-import jpasecurity.jpasecurity.model.dto.user.CreateUserDto;
-import jpasecurity.jpasecurity.model.dto.user.update.UpdateUserPasswordDto;
-import jpasecurity.jpasecurity.model.dto.user.update.UpdateUsernameDto;
-import jpasecurity.jpasecurity.model.dto.user.UserDto;
+import jpasecurity.jpasecurity.controller.user.CreateUserDto;
+import jpasecurity.jpasecurity.controller.user.UpdateUserPasswordDto;
+import jpasecurity.jpasecurity.controller.user.UserDto;
+import jpasecurity.jpasecurity.expcetion.EmailAddressIsTakenException;
 import jpasecurity.jpasecurity.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.repository.query.Param;
@@ -25,50 +22,41 @@ public class UserController {
 
     @GetMapping("/all")
     @PreAuthorize("hasRole('USER')")
-    public List<UserDto> getAllUsers(){
+    public List<UserDto> getAllUsers() {
         return userService.findAllUsers();
     }
 
+    @PreAuthorize("hasAuthority('SCOPE_ROLE_USER')")
+    @GetMapping("/user-details")
+    public UserDto getUserDetails() {
+        return userService.getUserDetails();
+    }
+
     @GetMapping("/{userId}")
-    @Operation(summary = "Return user's data")
     @ResponseStatus(HttpStatus.OK)
-    @ApiResponse(responseCode = "200")
     public UserDto getUserById(@PathVariable Long userId) {
         return userService.findUserById(userId);
     }
 
     @GetMapping("/name")
-    public UserDto getUserByUniqueName(@Param("username") String username){
+    public UserDto getUserByUniqueName(@Param("username") String username) {
         return userService.getUserByUniqueName(username);
     }
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    @Operation(summary = "Create new user")
-    @ApiResponse(responseCode = "201")
-    public UserDto createNewUser(@RequestBody @Valid CreateUserDto createUserDto) throws UsernameIsTakenException {
+    public UserDto createNewUser(@RequestBody @Valid CreateUserDto createUserDto) throws EmailAddressIsTakenException {
         return userService.saveNewUser(createUserDto);
     }
 
-    @PatchMapping("/{userId}/username")
+    @PatchMapping("/user/password")
     @ResponseStatus(HttpStatus.ACCEPTED)
-    @Operation(summary = "Update user's username")
-    @ApiResponse(responseCode = "202")
-    public void updateUsername(@RequestBody @Valid UpdateUsernameDto updateUsernameDto, @PathVariable Long userId) {
-        userService.updateUsername(userId, updateUsernameDto);
-    }
-
-    @PatchMapping("/{userId}/password")
-    @ResponseStatus(HttpStatus.ACCEPTED)
-    @Operation(summary = "Update user's password")
-    @ApiResponse(responseCode = "202")
-    public void updateUserPassword(@RequestBody @Valid UpdateUserPasswordDto updateUserPasswordDto, @PathVariable Long userId) {
-        userService.updateUserPassword(userId, updateUserPasswordDto);
+    public void updateUserPassword(@RequestBody @Valid UpdateUserPasswordDto updateUserPasswordDto) {
+        userService.updateUserPassword(updateUserPasswordDto);
     }
 
     @DeleteMapping("/{userId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    @Operation(summary = "Delete user by id")
-    @ApiResponse(responseCode = "204")
     public void deleteUserById(@PathVariable Long userId) {
         userService.deleteUserById(userId);
     }
--- src/main/java/jpasecurity/jpasecurity/expcetion/UserNotFoundException.java
+++ src/main/java/jpasecurity/jpasecurity/expcetion/UserNotFoundException.java
@@ -1,7 +1,6 @@
 package jpasecurity.jpasecurity.expcetion;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
 public class UserNotFoundException extends RuntimeException {
