ee742f2d4f66a7d16a0b708afc584d45030d4bee
szala <szalaytamas90@gmail.com>
2024-06-10 00:09:50 +0200

    refactor + forgotten password prepared

--- src/main/java/jpasecurity/jpasecurity/controller/AuthController.java
+++ src/main/java/jpasecurity/jpasecurity/controller/AuthController.java
@@ -1,13 +1,10 @@
 package jpasecurity.jpasecurity.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
-import jpasecurity.jpasecurity.model.dto.user.LoginUserDto;
-import jpasecurity.jpasecurity.model.dto.user.UserDto;
-import jpasecurity.jpasecurity.model.dto.user.UserRegistrationDto;
+import jpasecurity.jpasecurity.config.jwt.TokenService;
+import jpasecurity.jpasecurity.controller.user.LoginUserDto;
+import jpasecurity.jpasecurity.controller.user.UserDto;
+import jpasecurity.jpasecurity.controller.user.UserRegistrationDto;
 import jpasecurity.jpasecurity.service.AuthService;
-import jpasecurity.jpasecurity.service.TokenService;
 import jpasecurity.jpasecurity.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -23,13 +20,25 @@ public class AuthController {
 
     @PostMapping("/sign-in")
     @ResponseStatus(HttpStatus.ACCEPTED)
-    public String login(@RequestBody LoginUserDto loginUserDto, HttpServletRequest request, HttpServletResponse response, HttpSession session) {
-        return tokenService.generateToken(authService.authenticateLoginRequest(loginUserDto, session));
+    public String login(@RequestBody LoginUserDto loginUserDto) {
+        return tokenService.generateToken(authService.authenticateLoginRequest(loginUserDto));
     }
 
     @PostMapping("/registration")
     @ResponseStatus(HttpStatus.CREATED)
     public UserDto registeringNewUser(@RequestBody UserRegistrationDto userRegistrationDto) {
         return this.userService.registering(userRegistrationDto);
+    }
+
+    @PostMapping("/password-request")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void requestPassword(@RequestBody String emailAddress) {
+        userService.requestNewPassword(emailAddress);
+    }
+
+    @PostMapping("/reset-password")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void resetPassword(@RequestBody String pwResetCode) {
+
     }
 }
